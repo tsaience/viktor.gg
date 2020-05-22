6e091f1ff89a41aacef6b08586abcf9503254276
Daniel Rigobon <danielrigobon@Daniels-MacBook-Pro.local>
2020-05-22 11:22:32 -0400

    pca code

--- analysis.py
+++ analysis.py
@@ -14,7 +14,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
-
 def process_champ_name(champ_name):
 	split_by_space = "".join(champ_name.split(" "))
 	split_by_apostrophe = "".join(split_by_space.split("'"))
@@ -96,32 +95,60 @@ def extract_game_from_row(row_of_interest, verbose=True):
 	return game
 
 
-'''
+
+
+url = "http://ddragon.leagueoflegends.com/cdn/10.10.3216176/data/en_US/champion.json" 
+r = requests.get(url=url) 
+json_file = r.json()
+data = json_file['data']
+
+champ_names = list(data.keys())
+name = champ_names[0]
+cols = list(['name'])
+cols.extend(list(data[name]['stats'].keys()))
+
+df = pd.DataFrame(columns = cols)
+for name in champ_names:
+	dict_temp = dict()
+	dict_temp['name'] = data[name]['id']
+	dict_temp.update(data[name]['stats'])
+	df_temp = pd.DataFrame(dict_temp, index = [0])
+	df = df.append(df_temp, ignore_index = True)
+
+
 
 champ_chars = pd.read_csv('data/champ_chars.csv')
 champ_chars.name = [process_champ_name(champ_chars.name[i]) for i in range(len(champ_chars.index.values))]
 champ_chars.set_index('name', inplace = True)
 
 
+df.name = [process_champ_name(df.name[i]) for i in range(len(df.index.values))]
+df.set_index('name', inplace = True)
+
+
+df_full = pd.merge(df,champ_chars, on = 'name')
+
+df_full = champ_chars
+
+
+print(df_full.head())
+
+
 # scrap every row
-params = [('LPL','Spring',2020),('LCK','Spring',2020),('LEC','Spring',2020),('LCS','Spring',2020),
-		('LPL','Spring',2019),('LCK','Spring',2019),('LEC','Spring',2019),('LCS','Spring',2019),
-		('LPL','Summer',2019),('LCK','Summer',2019),('LEC','Summer',2019),('LCS','Summer',2019),
-		('LPL','Spring',2018),('LCK','Spring',2018)]
+params = [('LPL','Spring',2020),('LCK','Spring',2020),('LEC','Spring',2020),('LCS','Spring',2020)]#,
+#		('LPL','Spring',2019),('LCK','Spring',2019),('LEC','Spring',2019),('LCS','Spring',2019),
+#		('LPL','Summer',2019),('LCK','Summer',2019),('LEC','Summer',2019),('LCS','Summer',2019),
+#		('LPL','Spring',2018),('LCK','Spring',2018)]
 
-
-
-chars_cols = champ_chars.columns.values
+chars_cols = df_full.columns.values
 sides = ['blue','red']
 
-cols_1 = ['blue_'+name for name in chars_cols]
-cols_2 = ['red_'+name for name in chars_cols]
+cols_1 = ['blue_'+name for name in df_full]
+cols_2 = ['red_'+name for name in df_full]
 
 cols = ['blue_win']+cols_1+cols_2
 
-
 df_games = pd.DataFrame(columns = cols)
-
 
 for param in params:
 	url = construct_url_for_split(*param)
@@ -131,13 +158,13 @@ for param in params:
 
 		blue_champs = list(game.blue_champs)
 
-		blue_ind = np.in1d(champ_chars.index.values, blue_champs)
-		blue_mat = champ_chars.iloc[blue_ind]
+		blue_ind = np.in1d(df_full.index.values, blue_champs)
+		blue_mat = df_full.iloc[blue_ind]
 
 		red_champs = list(game.red_champs)
 
-		red_ind = np.in1d(champ_chars.index.values, red_champs)
-		red_mat = champ_chars.iloc[red_ind]
+		red_ind = np.in1d(df_full.index.values, red_champs)
+		red_mat = df_full.iloc[red_ind]
 
 		temp_b = blue_mat.sum(axis = 0)
 		temp_r = red_mat.sum(axis = 0)
@@ -148,18 +175,33 @@ for param in params:
 		df_games = df_games.append(df_temp, ignore_index = True)
 
 
-print(df_games)
+print(df_games.head())
 
 df_games.to_csv('data/game_stats.csv', index = False)
 
 
-'''
 
 
 
 df_games = pd.read_csv('data/game_stats.csv')
 
+# Perform PCA
+n_comp = 1
+
+pca = PCA(n_comp)
+scaler = StandardScaler()
+
+df_fitted = pd.DataFrame(pca.fit_transform(scaler.fit_transform(df_games.iloc[:,1:])),
+						 index = df_games.index, columns = ['PC'+str(i) for i in range(1,n_comp+1)])
+
+print(df_games.iloc[:,1:].head())
+print(df_fitted.head())
+
+X_pc = df_fitted.to_numpy()
+
 X = df_games.iloc[:,1:].to_numpy()
+X = X_pc
+
 y = df_games.blue_win.to_numpy()
 
 
@@ -170,19 +212,14 @@ lr_model = LogisticRegression()
 
 lr_model.fit(X_train, y_train)
 
+
 example_predict = lr_model.predict(X_test)
 
-print(lr_model.predict_proba(X_test))
 
+print(lr_model.predict_proba(X_test))
 print(lr_model.score(X_test,y_test))
 
-
-
-
-
-
-
-
+print(y.mean())
 
 
 
