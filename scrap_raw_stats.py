80c2c1ae6882b42b4271782ecb78bf7bd74ff370
drigobon <drigobon@gmail.com>
2020-09-22 16:22:38 -0400

    clustered winrate preliminary pipeline

--- scrap_raw_stats.py
+++ scrap_raw_stats.py
@@ -3,17 +3,24 @@
 import numpy as np
 import pandas as pd
 import random
+import json 
+import requests  
+import urllib.request
+
+import matplotlib.pyplot as plt
+
+from itertools import product
+
+from bs4 import BeautifulSoup
+
 from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram
-#import cassiopeia as cass
-import json 
-import requests  
-import urllib.request
-from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
-#import seaborn as sns
+
+
+# Our Code
+from models import LeagueGame
 
 
 
@@ -27,9 +34,6 @@ def process_names(champ_name):
     split_by_period = "".join(split_by_amp.split("."))    
     lowercase = split_by_period.lower()
     return lowercase
-
-
-
 
 
 def load_data(patch):
@@ -49,7 +53,6 @@ def load_data(patch):
     cols.extend(['tags'])
     
     df = pd.DataFrame(columns = cols)
-
     
     # Input champ data 
     for name in champ_names:
@@ -64,29 +67,20 @@ def load_data(patch):
         
         df = df.append(df_temp, ignore_index = True)
             
-        
     # Process champ names for consistency
     df.name = [process_names(df.name[i]) for i in range(len(df.index.values))]
-    
     df.set_index('name', inplace = True)
-    
     
     # One-hot encoding of riot's tags   
     mlb = MultiLabelBinarizer()
-    
     df_onehot = pd.DataFrame(mlb.fit_transform(df.loc[:,'tags']), columns = mlb.classes_, index = df.index)
-    
     df = df.drop('tags', axis = 1).merge(df_onehot, left_index = True, right_index = True)
-    
     df.columns = [process_names(item) for item in df.columns.values]
     
-        
     return (json_file, df, list(data.keys()))
 
 
-
 def get_core_items(champs_all, patch, champ_type = None):
-    
     
     items_all = dict()
     
@@ -144,20 +138,22 @@ def get_core_items(champs_all, patch, champ_type = None):
     return item_df
     
 
-
-
-
-def pca_analysis(df, n_comp, n_champs_print = 10, plot = False, show = False, scale = True):
+def pca_analysis(df, n_comp, n_champs_print = 10, plot = False, show = False, scale = True, scale_cols = None):
     
     pca = PCA(n_comp)
 
+    if scale_cols is None:
+        scale_cols = df.columns.values
+
     if scale:
         scaler = StandardScaler()
-        temp = pca.fit_transform(scaler.fit_transform(df))
+        df_temp = pd.DataFrame(scaler.fit_transform(df.loc[:,scale_cols]), index = df.index.values, columns = df.loc[:,scale_cols].columns.values)
+        
+        df = df_temp.merge(df.drop(scale_cols, axis = 1), how = 'inner', left_index = True, right_index = True)
+        
 
-    else:
-        temp = pca.fit_transform(df)
-    
+
+    temp = pca.fit_transform(df)
     
     # Data of output matrix
     df_fitted = pd.DataFrame(temp, index = df.index,
@@ -187,10 +183,6 @@ def pca_analysis(df, n_comp, n_champs_print = 10, plot = False, show = False, sc
     return df_fitted
     
 
-
-
-
-
 def fit_clusters(df, figname, n_clusters = 3, plot = False, show = False, n_init = 50, clstr = 'kmeans'):
     
     colors = ['red','blue','green','orange','yellow','brown','gray']
@@ -219,10 +211,7 @@ def fit_clusters(df, figname, n_clusters = 3, plot = False, show = False, n_init
             plt.savefig('fig/'+figname+'.pdf')
             plt.show()
         
-        
         return (model,df_out)
-    
-    
     
     elif clstr == 'agg':
         
@@ -233,7 +222,6 @@ def fit_clusters(df, figname, n_clusters = 3, plot = False, show = False, n_init
             
             if plot:
                 plot_dendrogram(model, figname, labels = df.index.values)
-            
             
             return (model, df)
         
@@ -258,19 +246,12 @@ def fit_clusters(df, figname, n_clusters = 3, plot = False, show = False, n_init
                 plt.savefig('fig/'+figname+'.pdf')
                 plt.show()
             
-            
-            return (model, df)
+            return (model, df_out)
         
     else:
         Exception('Unrecognized clustering type')
     
     
-    
-    
-    
-    
-
-
 def plot_dendrogram(model, figname, **kwargs):
     # Create linkage matrix and then plot the dendrogram
     f,ax = plt.subplots(figsize = (10,7))
@@ -302,29 +283,56 @@ def plot_dendrogram(model, figname, **kwargs):
 
 
 def run_clustering_analysis(df, item_df_jung, item_df_lane, roles, role_names, 
-                            n_clusters = 2, clstr = 'kmeans', n_comp = 6, 
-                            n_champs_print = 5, plot = True, show = False, scale = True, n_init = 50):
+                            n_clusters = [2,2,2,2,2], clstr = 'kmeans', n_comp = 6, 
+                            n_champs_print = 5, plot = True, show = False, 
+                            scale = True, scale_cols = None, n_init = 50):
+    
+    cluster_dicts = dict()
+    
+    for i,role in enumerate(roles):
+    
+        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        print(role_names[i])
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        
+        if role_names[i]=='Jungle':
+            df_items = item_df_jung.loc[role,:].dropna(axis = 0)
+        else:
+            df_items = item_df_lane.loc[role,:].dropna(axis = 0)
+        
+        df_champ = df.loc[role,:].dropna(axis = 0)
+        
+        df_temp = df_champ#.merge(df_items, how = 'outer', left_index = True, right_index = True)
+        
+        df_fitted = pca_analysis(df_temp, n_comp, n_champs_print, False, show, scale, scale_cols)
+        
+        # do clustering
+        (model,df_clusters) = fit_clusters(df_fitted, role_names[i], n_clusters[i], plot, show, n_init, clstr = clstr)
+    
+        cluster_dicts[role_names[i]] = df_clusters.to_dict()['labels']
+    
+    # Save clustering dict
+    with open('clusters.json','w') as fp:
+        json.dump(cluster_dicts, fp)
+        
+    return cluster_dicts
+
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
     
 
-    return
 
-
-
-###################################################################################################
-###################################################################################################
-###################################################################################################
-    
-
-
-
+# Get all Champ and Item Data
 (json_file, df, champ_list) = load_data(patch = "10.19.1")
-
 item_df_jung = get_core_items(champ_list, patch = "10.19.1", champ_type = 'jungle')
 item_df_lane = get_core_items(champ_list, patch = "10.19.1", champ_type = None)
 
 
 
-
+# Manual Input Here
 tops = ['camille','shen','jax','renekton','darius','garen','fiora','mordekaiser','irelia','riven','malphite','sett','akali','wukong','aatrox','yone','maokai','jayce','vladimir','ornn','tryndamere','volibear','sylas','gangplank','teemo','nasus','urgot','gnar','poppy','kayle','yasuo','lucian','sion','singed','kled','kennen','illaoi','chogath','yorick','rengar','hecarim','lillia','vayne','drmundo','quinn','rumble']
 jungs = ['leesin','kayn','khazix','ekko','graves','hecarim','evelynn','elise','nunuwillump','zac','nidalee','volibear','masteryi','lillia','shaco','fiddlesticks','karthus','vi','kindred','olaf','reksai','sylas','warwick','rengar','shyvana','jarvaniv','sett','nocturne','amumu','gragas','rammus','sejuani','ivern','udyr','skarner','jax','xinzhao','trundle']
 mids = ['yone', 'zed', 'yasuo', 'akali', 'sylas', 'ahri', 'orianna', 'kassadin', 'katarina', 'zoe', 'fizz', 'galio', 'vladimir', 'ekko', 'leblanc', 'cassiopeia', 'talon', 'diana', 'twistedfate', 'lux', 'irelia', 'syndra', 'veigar', 'lucian', 'qiyana', 'annie', 'malzahar', 'pantheon', 'viktor', 'lissandra', 'azir', 'velkoz', 'ryze', 'ziggs', 'xerath', 'neeko', 'pyke', 'anivia', 'swain', 'renekton', 'sett', 'corki']
@@ -334,46 +342,27 @@ sups = ['thresh', 'lulu', 'lux', 'morgana', 'senna', 'yuumi', 'nautilus', 'karma
 
 
 
+# Parameters for Clustering
 roles = [tops, jungs, mids, adcs, sups]
 role_names = ['Top','Jungle','Mid','ADC','Support']
 
 
-
-
-
-n_comp = 8
+n_comp = 10
 n_champs_print = 5
 plot = True
 show = False
-scale = False
+scale = True
 clstr = 'agg'
-
 n_clusters = [3,2,2,3,2]
 n_init = 50
+scale_cols = df.columns.values[:20] #all base and scaling stats
 
-for i,role in enumerate(roles):
-    
-    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    print(role_names[i])
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    
-    if role_names[i]=='Jungle':
-        df_items = item_df_jung.loc[role,:].dropna(axis = 0)
-    else:
-        df_items = item_df_lane.loc[role,:].dropna(axis = 0)
-    
-    
-    df_champ = df.loc[role,:].dropna(axis = 0)
-    
-    df_temp = df_champ.merge(df_items, how = 'outer', left_index = True, right_index = True)
-    
-    
-    df_fitted = pca_analysis(df_temp, n_comp, n_champs_print, False, show, scale)
-    
-    
-    # do clustering
-    (model,df_clusters) = fit_clusters(df_fitted, role_names[i], n_clusters[i], plot, show, n_init, clstr = clstr)
 
+# Run Clustering Analysis
+cluster_dicts = run_clustering_analysis(df, item_df_jung, item_df_lane, roles, role_names, 
+                            n_clusters = n_clusters, clstr = clstr, n_comp = n_comp, 
+                            n_champs_print = n_champs_print, plot = plot, show = show,
+                            scale = scale, scale_cols = scale_cols, n_init = n_init)
 
 
 
@@ -382,322 +371,42 @@ for i,role in enumerate(roles):
 
 
 
+###################################################################################################
+###################################################################################################
+###################################################################################################
 
 
+clusters = [np.unique(list(cluster_dicts[name].values())).tolist() for name in role_names]
 
+team_clusters = list(product(*clusters))
 
+index = ["".join([str(idx) for idx in curr]) for curr in team_clusters]
 
+team_wins = pd.DataFrame(np.zeros((len(index),len(index))), index = index, columns = index)
+team_games = pd.DataFrame(np.zeros((len(index),len(index))), index = index, columns = index)
 
 
 
-'''
-champ_chars = pd.read_csv('data/champ_chars.csv')
 
+# Loop over Games
+game = LeagueGame(['ornn','leesin','syndra','ashe','nautilus'],['gangplank','nunuwillump','ekko','caitlyn','lux'],True, "10.19.1")
 
-df_full = pd.merge(df,champ_chars, on = 'name')
+blueid = "".join([str(cluster_dicts[role_names[i]][champ]) for i,champ in enumerate(game.blue_champs)])
+redid = "".join([str(cluster_dicts[role_names[i]][champ]) for i,champ in enumerate(game.red_champs)])
 
+team_games.loc[blueid,redid] = team_games.loc[blueid,redid] + 1 
+if game.blue_win:
+    team_wins.loc[blueid,redid] = team_wins.loc[blueid,redid] + 1 
+        
+# End Loop
 
-df_full = champ_chars
 
+team_winrate = team_wins/team_games
 
-def process_champ_name(champ_name):
-	split_by_space = "".join(champ_name.split(" "))
-	split_by_apostrophe = "".join(split_by_space.split("'"))
-	split_by_amp = "".join(split_by_apostrophe.split("&"))
-	lowercase = split_by_amp.lower()
-	return lowercase
 
-df_full.name = [process_champ_name(df_full.name[i]) for i in range(len(df_full.index.values))]
 
 
-df_full.set_index('name', inplace = True)
 
-#print(df_full)
-
-
-
-# Check Variance
-for col in df_full.columns:
-	if np.var(df_full.loc[:,col]) < 0.01:
-		df_full.drop(col, axis = 1, inplace = True)
-		print(f'Dropped {col}')
-
-
-
-
-
-# Perform PCA
-n_comp = 5
-
-pca = PCA(n_comp)
-scaler = StandardScaler()
-
-df_fitted = pd.DataFrame(pca.fit_transform(scaler.fit_transform(df_full)),
-						 index = df_full.index, columns = ['PC'+str(i) for i in range(1,n_comp+1)])
-
-
-
-for col in df_fitted.columns:
-	temp = df_fitted.loc[:,col] 
-
-	print(f'\n{col}: Top Champions')
-	print(temp.sort_values(ascending = False).iloc[0:10])
-
-
-	print(f'\n{col}: Lowest Champions')
-	print(temp.sort_values(ascending = True).iloc[0:10])
-
-
-
-
-
-def eff_rank(A):
-	AtA = np.dot(np.transpose(A),A)
-
-	return np.trace(AtA)/np.linalg.norm(A, ord = 2)
-
-
-
-#data structure
-
-class LeagueGame:
-	def __init__(self, blue_champs, red_champs, blue_win, patch):
-		self.blue_champs = blue_champs
-		self.red_champs = red_champs
-		self.blue_win = blue_win
-		self.patch = patch
-
-
-# league: 'LEC', 'LCS', 'LPL', 'LCK'
-# year: self-explanatory
-# split: 'Spring', 'Summer'
-
-def construct_url_for_split(league, split, year):
-	url = "https://lol.gamepedia.com/" + league + "/" + str(year) + "_Season/" + split + "_Season/Picks_and_Bans"
-	return url
-
-
-def extract_table_rows_from_url(url):
-	fp = urllib.request.urlopen(url)
-	page_bytes = fp.read()
-	page_html = page_bytes.decode("utf8")
-	fp.close()
-	soup = BeautifulSoup(page_html, 'html.parser')
-	draft_table = soup.find_all('table', id="pbh-table")[0]
-	table_rows = draft_table.find_all('tr')[2:]
-	return table_rows
-
-
-
-
-def extract_game_from_row(row_of_interest, verbose=True):
-
-	table_columns = row_of_interest.find_all('td')
-	blue, red = table_columns[1], table_columns[2]
-
-	blue_win = False
-	if blue.get("class") != None and blue.get("class")[0] == "pbh-winner":
-		blue_win = True
-
-	patch = table_columns[4].get_text()
-
-	champs_columns = table_columns[11:15] + table_columns[19:22]
-
-	# bp1, rp1-2, bp2-3, rp3, rp4, bp4-5, rp5
-
-	non_champ_classes = {'pbh-blue', 'pbh-cell', 'pbh-red', 'pbh-divider'}
-
-	blue_champs = set()
-	red_champs = set()
-
-	for i in range(len(champs_columns)):
-		column = champs_columns[i]
-		classes = column.get("class")
-		for column_class in classes:
-			if column_class not in non_champ_classes:
-				champ = column_class.split("-")[1]
-				if i in {0, 2, 5}:
-					blue_champs.add(champ)
-				else:
-					red_champs.add(champ)
-
-	game = LeagueGame(blue_champs, red_champs, blue_win, patch)
-
-	if verbose:
-		print("game blue champs: ", game.blue_champs)
-		print("game red champs: ", game.red_champs)
-		print("game blue win: ", game.blue_win)
-		print("game patch: ", game.patch)
-		print()
-
-	return game
-
-
-
-
-
-
-
-# scrap every row
-params = [('LPL','Spring',2020)]#,('LCK','Spring',2020),('LEC','Spring',2020),('LCS','Spring',2020),
-#		('LPL','Spring',2019),('LCK','Spring',2019),('LEC','Spring',2019),('LCS','Spring',2019),
-#		('LPL','Summer',2019),('LCK','Summer',2019),('LEC','Summer',2019),('LCS','Summer',2019),
-#		('LPL','Spring',2018),('LCK','Spring',2018)]
-
-
-
-chars_cols = champ_chars.columns.values
-sides = ['blue','red']
-
-cols_1 = ['blue_'+name for name in chars_cols]
-cols_2 = ['red_'+name for name in chars_cols]
-
-cols = ['blue_win']+cols_1+cols_2
-
-
-df_games = pd.DataFrame(columns = cols)
-
-
-for param in params:
-	url = construct_url_for_split(*param)
-	table_rows = extract_table_rows_from_url(url)
-	for row in table_rows:
-		game = extract_game_from_row(row, verbose = False) 
-
-
-		blue_champs = list(game.blue_champs)
-
-		blue_ind = np.in1d(champ_chars.index.values, blue_champs)
-		blue_mat = champ_chars.iloc[blue_ind]
-
-		red_champs = list(game.red_champs)
-
-		red_ind = np.in1d(champ_chars.index.values, red_champs)
-		red_mat = champ_chars.iloc[red_ind]
-
-		#print(blue_mat)
-		#print(red_mat)
-
-		temp_b = blue_mat.sum(axis = 0)
-		temp_r = red_mat.sum(axis = 0)
-
-
-		df_temp = pd.DataFrame([[int(game.blue_win)] + temp_b.values.tolist() + temp_r.values.tolist()], columns = cols)
-
-		df_games = df_games.append(df_temp, ignore_index = True)
-
-
-print(df_games)
-
-'''
-
-
-
-
-
-
-
-
-
-
-'''
-
-df_score = pd.DataFrame(columns = ['score', 'win'])
-
-
-
-# scrap every row
-url = construct_url_for_split("LCK", "Summer", 2018)
-table_rows = extract_table_rows_from_url(url)
-for row in table_rows:
-	game = extract_game_from_row(row, verbose = False) # set this to false if you dont want to see prints
-
-
-	## THIS IS WHERE WE DEFINE A SCORE FUNCTION
-
-	blue_champs = list(game.blue_champs)
-
-	blue_ind = np.in1d(df_fitted.index.values, blue_champs)
-	blue_mat = df_fitted.iloc[blue_ind]
-
-	blue_score = eff_rank(blue_mat)
-
-	df_temp = pd.DataFrame({'score': blue_score, 'win': game.blue_win}, index = [0])
-
-	df_score = df_score.append(df_temp, ignore_index = True)
-
-
-print(df_score)
-
-
-
-
-n_buckets = 20
-
-ticks = np.linspace(min(df_score.score), max(df_score.score), n_buckets)
-
-x = np.zeros(n_buckets-1)
-y = np.zeros(n_buckets-1)
-
-for i in range(len(ticks)-1):
-	x[i] = 0.5*(ticks[i]+ticks[i+1])
-
-	ind = (ticks[i] <= df_score.score) & (df_score.score <= ticks[i+1])
-
-	y[i] = df_score.win.loc[ind].mean()
-
-print(x)
-print(y)
-
-
-f,ax = plt.subplots(nrows = 1, ncols = 1, figsize = (7,7))
-
-sns.pointplot(x = x, y = y)
-
-plt.savefig('temp.pdf')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-red_champs = ['ornn', 'leesin', 'anivia', 'kaisa', 'nautilus']
-
-blue_champs = ['jayce', 'gragas', 'zoe', 'corki', 'janna']
-
-
-red_ind = np.in1d(df_fitted.index.values, red_champs)
-red_mat = df_fitted.iloc[red_ind]
-
-print(red_mat)
-
-print(eff_rank(red_mat))
-
-
-
-blue_ind = np.in1d(df_fitted.index.values, blue_champs)
-blue_mat = df_fitted.iloc[blue_ind]
-
-print(blue_mat)
-
-print(eff_rank(blue_mat))
 
 
 
